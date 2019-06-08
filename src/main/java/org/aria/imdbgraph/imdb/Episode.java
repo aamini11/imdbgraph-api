3d906eace25d5e042607056ecd6cf2a182d2f79a
Aria Amini <amini5454@gmail.com>
2019-06-08 03:14:05 -0700

    More docs

--- src/main/java/org/aria/imdbgraph/imdb/Episode.java
+++ src/main/java/org/aria/imdbgraph/imdb/Episode.java
@@ -1,5 +1,8 @@
 package org.aria.imdbgraph.imdb;
 
+/**
+ * Data class to hold information about a specific episode of a show include the IMDB ratings for that episode.
+ */
 public final class Episode {
 
     private final String episodeTitle;
@@ -8,6 +11,14 @@ public final class Episode {
     private final double imdbRating;
     private final int numVotes;
 
+    /**
+     * Constructor used to initialize episode information
+     * @param episodeTitle Title of the episode
+     * @param season Season number of episode
+     * @param episodeNumber Episode number of episode
+     * @param imdbRating The average IMDB rating of the episode (0.0 - 10.0)
+     * @param numVotes Total number of votes for the episode (> 0)
+     */
     Episode(String episodeTitle, int season, int episodeNumber, double imdbRating, int numVotes) {
         this.episodeTitle = episodeTitle;
         this.season = season;
