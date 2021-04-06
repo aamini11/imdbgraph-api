cd471587706a4cd9e77d82152d5207ba61cc686e
Aria <amini5454@gmail.com>
2021-04-06 03:32:07 -0500

    Fix testing of scrapper

--- src/test/java/org/aria/imdbgraph/scrapper/DatabaseLoaderUnitIT.java
+++ src/test/java/org/aria/imdbgraph/scrapper/DatabaseLoaderUnitIT.java
@@ -1,12 +1,16 @@
 package org.aria.imdbgraph.scrapper;
 
 import org.aria.imdbgraph.scrapper.DatabaseUpdater.ImdbFileParsingError;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.jdbc.JdbcTestUtils;
@@ -30,12 +34,18 @@ import static org.mockito.Mockito.when;
 class DatabaseLoaderUnitIT {
 
     private static final Path SAMPLE_FILES_DIR = Paths.get("src/test/resources/samples-files");
+    private static Path INPUT_DIR;
+    private static Path ARCHIVE_DIR;
 
-    @TempDir
-    Path workspace;
-
-    private Path input;
-    private Path archive;
+    @TestConfiguration
+    @Profile("test")
+    static class ScrapperTestConfiguration {
+        @Bean
+        FileArchiver testArchiver() {
+            Instant instant = Instant.parse("2007-12-03T10:15:30.00Z");
+            return new FileArchiver(ARCHIVE_DIR, Clock.fixed(instant, ZoneId.of("America/Chicago")));
+        }
+    }
 
     @Autowired
     private JdbcTemplate jdbc;
@@ -43,26 +53,42 @@ class DatabaseLoaderUnitIT {
     @MockBean
     private ImdbFileService fileService;
 
-    private DatabaseUpdater databaseUpdater;
+    @Autowired
+    private DatabaseUpdater testDatabaseUpdater;
+
+    @BeforeAll
+    static void setUpDirectories() throws IOException {
+        Path TEMP_DIR = Files.createTempDirectory("temp");
+        INPUT_DIR = Files.createDirectory(TEMP_DIR.resolve("input_files"));
+        ARCHIVE_DIR = Files.createDirectory(TEMP_DIR.resolve("archive"));
+    }
 
     @BeforeEach
-    void setUp() throws IOException {
-        setUpDirectories();
-        loadSampleFiles();
-        cleanDb();
+    void loadFiles() throws IOException {
+        File[] sampleFiles = SAMPLE_FILES_DIR.toFile().listFiles();
+        Objects.requireNonNull(sampleFiles, SAMPLE_FILES_DIR + " not found");
+        for (File sample : sampleFiles) {
+            Files.copy(sample.toPath(), INPUT_DIR.resolve(sample.getName()));
+        }
+    }
 
-        Instant instant = Instant.parse("2007-12-03T10:15:30.00Z");
-        FileArchiver archiver = new FileArchiver(archive, Clock.fixed(instant, ZoneId.of("America/Chicago")));
-        databaseUpdater = new DatabaseUpdater(jdbc, fileService, archiver);
+    @AfterEach
+    void cleanUp() throws IOException {
+        // Wipe database
+        JdbcTestUtils.deleteFromTables(jdbc, "imdb.episode", "imdb.show");
+
+        // Clean directories of previous files
+        cleanDirectory(INPUT_DIR);
+        cleanDirectory(ARCHIVE_DIR);
     }
 
     @Test
     void testSampleFiles() throws ImdbFileParsingError {
-        when(fileService.download(TITLES_FILE)).thenReturn(input.resolve("title_sample.tsv"));
-        when(fileService.download(RATINGS_FILE)).thenReturn(input.resolve("ratings_sample.tsv"));
-        when(fileService.download(EPISODES_FILE)).thenReturn(input.resolve("episode_sample.tsv"));
+        when(fileService.download(TITLES_FILE)).thenReturn(INPUT_DIR.resolve("title_sample.tsv"));
+        when(fileService.download(RATINGS_FILE)).thenReturn(INPUT_DIR.resolve("ratings_sample.tsv"));
+        when(fileService.download(EPISODES_FILE)).thenReturn(INPUT_DIR.resolve("episode_sample.tsv"));
 
-        databaseUpdater.updateDatabase();
+        testDatabaseUpdater.updateDatabase();
         int epCount = JdbcTestUtils.countRowsInTable(jdbc, "imdb.episode");
         int showCount = JdbcTestUtils.countRowsInTable(jdbc, "imdb.show");
         assertEquals(3, showCount);
@@ -71,32 +97,30 @@ class DatabaseLoaderUnitIT {
 
     @Test
     void testBadFiles() {
-        when(fileService.download(TITLES_FILE)).thenReturn(input.resolve("title_sample.tsv"));
-        when(fileService.download(RATINGS_FILE)).thenReturn(input.resolve("ratings_sample.tsv"));
-        when(fileService.download(EPISODES_FILE)).thenReturn(input.resolve("bad_episode_sample.tsv"));
+        when(fileService.download(TITLES_FILE)).thenReturn(INPUT_DIR.resolve("title_sample.tsv"));
+        when(fileService.download(RATINGS_FILE)).thenReturn(INPUT_DIR.resolve("ratings_sample.tsv"));
+        when(fileService.download(EPISODES_FILE)).thenReturn(INPUT_DIR.resolve("bad_episode_sample.tsv"));
 
-        assertThrows(ImdbFileParsingError.class, () -> databaseUpdater.updateDatabase());
+        assertThrows(ImdbFileParsingError.class, () -> testDatabaseUpdater.updateDatabase());
 
-        File[] archivedFiles = archive.toFile().listFiles();
+        File[] archivedFiles = ARCHIVE_DIR.toFile().listFiles();
         assertNotNull(archivedFiles);
         assertEquals(1, archivedFiles.length);
-        assertEquals("title_sample_2007-12-03.tsv", archivedFiles[0].getName());
+        assertEquals("bad_episode_sample_2007-12-03.tsv", archivedFiles[0].getName());
     }
 
-    private void setUpDirectories() throws IOException {
-        input = Files.createDirectory(workspace.resolve("input"));
-        archive = Files.createDirectory(workspace.resolve("archive"));
-    }
-
-    private void loadSampleFiles() throws IOException {
-        File[] sampleFiles = SAMPLE_FILES_DIR.toFile().listFiles();
-        Objects.requireNonNull(sampleFiles, SAMPLE_FILES_DIR + " not found");
-        for (File sample : sampleFiles) {
-            Files.copy(sample.toPath(), input.resolve(sample.getName()));
+    /**
+     * Recursively delete all files but keep all directories intact
+     */
+    private static void cleanDirectory(Path root) throws IOException {
+        File[] files = root.toFile().listFiles();
+        Objects.requireNonNull(files, root + " not found");
+        for(File f: files) {
+            if(f.isDirectory()) {
+                cleanDirectory(f.toPath());
+            } else {
+                Files.deleteIfExists(f.toPath());
+            }
         }
-    }
-
-    private void cleanDb() {
-        JdbcTestUtils.deleteFromTables(jdbc, "imdb.episode", "imdb.show");
     }
 }
