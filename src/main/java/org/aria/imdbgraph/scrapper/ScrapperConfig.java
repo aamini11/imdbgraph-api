739bbbe3ce473022dace4184ff89bde87a5b0920
Aria <amini5454@gmail.com>
2021-04-06 03:17:28 -0500

    Add spring config file to set up bean definitions for the IMDB scrapper

---
+++ src/main/java/org/aria/imdbgraph/scrapper/ScrapperConfig.java
@@ -0,0 +1,29 @@
+package org.aria.imdbgraph.scrapper;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Clock;
+import java.time.ZoneId;
+
+@Configuration
+class ScrapperConfig {
+
+    @Bean
+    ImdbFileService fileService(@Value("${imdbgraph.data.directory}")
+                                        String downloadDirPath) {
+        Path downloadDir = Paths.get(downloadDirPath);
+        return new ImdbFileService(downloadDir);
+    }
+
+    @Bean
+    FileArchiver archiver(@Value("${imdbgraph.data.directory}")
+                                  String downloadDirPath) {
+        Path source = Paths.get(downloadDirPath);
+        Clock texasClock = Clock.system(ZoneId.of("US/Central"));
+        return new FileArchiver(source.resolve("archive"), texasClock);
+    }
+}
