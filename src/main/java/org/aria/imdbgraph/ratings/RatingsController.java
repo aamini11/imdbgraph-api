b9dd3cd84deaf3f2ba90bec99daac0c393939e8f
Aria <amini5454@gmail.com>
2022-01-19 20:34:33 -0600

    Refactor to REST API

--- src/main/java/org/aria/imdbgraph/ratings/RatingsController.java
+++ src/main/java/org/aria/imdbgraph/ratings/RatingsController.java
@@ -2,45 +2,36 @@ package org.aria.imdbgraph.ratings;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController("/api")
 class RatingsController {
 
-    private final ImdbRatingsService imdbRatingsService;
+    private final RatingsService ratingsService;
 
     @Autowired
-    RatingsController(ImdbRatingsService imdbRatingsService) {
-        this.imdbRatingsService = imdbRatingsService;
-    }
-
-    @GetMapping(value = "/")
-    public String index() {
-        return "search_results";
+    RatingsController(RatingsService ratingsService) {
+        this.ratingsService = ratingsService;
     }
 
     @GetMapping(value = "/ratings/{showId}")
-    public String getRating(@PathVariable(value = "showId") String showId, Model model) {
-        Optional<RatingsGraph> ratings = imdbRatingsService.getAllShowRatings(showId);
+    public Ratings getRatings(@PathVariable(value = "showId") String showId) {
+        Optional<Ratings> ratings = ratingsService.getAllShowRatings(showId);
         if (ratings.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Show not found");
         }
-        model.addAttribute("ratingsData", ratings.get());
-        return "rating_page";
+        return ratings.get();
     }
 
     @GetMapping(value = "/search")
-    public String search(@RequestParam(value="q") String searchTerm, Model model) {
-        List<Show> shows = imdbRatingsService.searchShows(searchTerm);
-        model.addAttribute("results", shows);
-        return "search_results";
+    public List<Show> search(@RequestParam(value="q") String searchTerm) {
+        return ratingsService.searchShows(searchTerm);
     }
 }
