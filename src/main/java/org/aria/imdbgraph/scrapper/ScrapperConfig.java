41c63be3515b8a89dff4de4f43fb811533bcefec
Aria <amini5454@gmail.com>
2022-03-07 02:32:41 -0600

    Tiny refactor to extract variable

--- src/main/java/org/aria/imdbgraph/scrapper/ScrapperConfig.java
+++ src/main/java/org/aria/imdbgraph/scrapper/ScrapperConfig.java
@@ -1,5 +1,6 @@
 package org.aria.imdbgraph.scrapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,16 +13,21 @@ import java.time.ZoneId;
 @Configuration
 class ScrapperConfig {
 
+    private final String downloadDirPath;
+
+    @Autowired
+    public ScrapperConfig(@Value("${imdbgraph.data.directory}") String downloadDirPath) {
+        this.downloadDirPath = downloadDirPath;
+    }
+
     @Bean
-    ImdbFileDownloader imdbFileDownloader(@Value("${imdbgraph.data.directory}")
-                                        String downloadDirPath) {
+    ImdbFileDownloader imdbFileDownloader() {
         Path downloadDir = Paths.get(downloadDirPath);
         return new ImdbFileDownloader(downloadDir);
     }
 
     @Bean
-    FileArchiver archiver(@Value("${imdbgraph.data.directory}")
-                                  String downloadDirPath) {
+    FileArchiver archiver() {
         Path source = Paths.get(downloadDirPath);
         Clock texasClock = Clock.system(ZoneId.of("US/Central"));
         return new FileArchiver(source.resolve("archive"), texasClock);
