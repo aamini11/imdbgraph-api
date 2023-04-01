0a581b111eaf3c743f4fb4812f7307f0b3039cfd
Aria <amini5454@gmail.com>
2023-04-01 19:06:47 -0400

    Ansible refactors

--- infra/roles/web/templates/start.sh
+++ infra/roles/web/templates/start.sh
@@ -1,1 +1,6 @@
-java -jar imdbgraph-api
+export POSTGRES_HOST="{{ lookup('env', 'POSTGRES_HOST' }}"
+export POSTGRES_DB="{{ lookup('env', 'POSTGRES_DB' }}"
+export POSTGRES_USER="{{ lookup('env', 'POSTGRES_USER' }}"
+export POSTGRES_PASSWORD="{{ lookup('env', 'POSTGRES_PASSWORD' }}"
+
+java -jar imdbgraph-0.0.1-SNAPSHOT.jar
