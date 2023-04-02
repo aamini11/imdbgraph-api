cf350fdef339b03cd296783113e49c78448fff73
Aria <amini5454@gmail.com>
2023-04-02 03:25:05 -0400

    Fix broken script

--- infra/roles/web/templates/start.sh
+++ infra/roles/web/templates/start.sh
@@ -5,4 +5,5 @@ export DATABASE_DB="{{ lookup('env', 'DATABASE_DB') }}"
 export DATABASE_USER="{{ lookup('env', 'DATABASE_USER') }}"
 export DATABASE_PASSWORD="{{ lookup('env', 'DATABASE_PASSWORD') }}"
 
+cd "home/{{ ansible_user }}"
 java -jar imdbgraph-0.0.1-SNAPSHOT.jar
