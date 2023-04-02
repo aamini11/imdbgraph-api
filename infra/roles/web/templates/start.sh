aaf5d7b2cb2ac817d050d9a93d83cfbd47e36553
Aria <amini5454@gmail.com>
2023-04-02 00:55:16 -0400

    Rename environment variables

--- infra/roles/web/templates/start.sh
+++ infra/roles/web/templates/start.sh
@@ -1,6 +1,6 @@
-export POSTGRES_HOST="{{ lookup('env', 'POSTGRES_HOST') }}"
-export POSTGRES_DB="{{ lookup('env', 'POSTGRES_DB') }}"
-export POSTGRES_USER="{{ lookup('env', 'POSTGRES_USER') }}"
-export POSTGRES_PASSWORD="{{ lookup('env', 'POSTGRES_PASSWORD') }}"
+export DATABASE_HOST="{{ lookup('env', 'DATABASE_HOST') }}"
+export DATABASE_DB="{{ lookup('env', 'DATABASE_DB') }}"
+export DATABASE_USER="{{ lookup('env', 'DATABASE_USER') }}"
+export DATABASE_PASSWORD="{{ lookup('env', 'DATABASE_PASSWORD') }}"
 
 java -jar imdbgraph-0.0.1-SNAPSHOT.jar
