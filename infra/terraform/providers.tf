84d24137391fbf61b4e9a25003767ebc9eb6341b
Aria <amini5454@gmail.com>
2025-01-06 16:10:23 -0500

    Try out something

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -1,4 +1,15 @@
 terraform {
+  required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "2.2.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.14.0"
+    }
+  }
+
   // Also requires manually setting up credentials for Azure:
   // https://developer.hashicorp.com/terraform/tutorials/azure-get-started/azure-remote#update-the-hcp-terraform-environment-variables
   cloud {
@@ -10,4 +21,8 @@ terraform {
       project = "imdbgraph-api"
     }
   }
+}
+
+provider azurerm {
+  features {}
 }
