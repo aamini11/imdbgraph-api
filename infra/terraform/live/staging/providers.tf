30222e6fa6dc2e5e722af9687fa6fa1f446ce619
Aria <amini5454@gmail.com>
2025-01-07 22:26:05 -0500

    Test

--- infra/terraform/live/staging/providers.tf
+++ infra/terraform/live/staging/providers.tf
@@ -2,12 +2,19 @@ terraform {
   required_providers {
     azapi = {
       source  = "Azure/azapi"
-      version = "2.2.0"
+      version = "~>2.2.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.14.0"
+      version = "~>4.14.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstateifgo5z83pc"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
