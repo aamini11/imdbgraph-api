fd2778fec6d4860c5d1182adeb9a281bc1a0b039
Aria <amini5454@gmail.com>
2025-01-09 18:59:02 -0500

    Clean up

--- src/main/java/org/aria/imdbgraph/api/ratings/ImdbFileDownloader.java
+++ src/main/java/org/aria/imdbgraph/api/ratings/ImdbFileDownloader.java
@@ -3,17 +3,15 @@ package org.aria.imdbgraph.api.ratings;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.*;
-import java.net.MalformedURLException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.net.URI;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.zip.GZIPInputStream;
+
+import static java.nio.file.Files.copy;
+import static java.nio.file.Files.createTempFile;
 
 /**
  * Utility class used by {@link ImdbDataScraper} to download the latest data
@@ -24,66 +22,39 @@ import java.util.zip.GZIPInputStream;
 @Service
 public final class ImdbFileDownloader {
 
-    private static final Logger logger = LogManager.getLogger();
-    private static final String BASE_DOWNLOAD_URL = "https://datasets.imdbws.com";
+    private static final Logger log = LogManager.getLogger();
+
+    private static final String DOWNLOAD_URL = "https://datasets.imdbws.com";
+
+    /**
+     * Downloads and unzips an IMDB data file and return the Path of the file if
+     * succesful.
+     */
+    public Path download(ImdbFile file) {
+        try (InputStream in = file.uri().toURL().openStream()) {
+            Path out = createTempFile(file.name, ".tmp");
+            copy(in, out);
+
+            log.info("Downloaded file: {} to {}", file.uri(), out);
+            return out;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 
     public enum ImdbFile {
         TITLES("title.basics.tsv.gz"),
         EPISODES("title.episode.tsv.gz"),
         RATINGS("title.ratings.tsv.gz");
 
-        private final String fileName;
+        private final String name;
 
-        ImdbFile(String fileName) {
-            this.fileName = fileName;
+        ImdbFile(String name) {
+            this.name = name;
         }
 
-        /**
-         * Remove .gz file extension from file name.
-         */
-        private String getUnzippedFileName() {
-            return fileName.substring(0, fileName.lastIndexOf('.'));
-        }
-
-        private URL getDownloadUrl() {
-            URI downloadUri = UriComponentsBuilder
-                    .fromUriString(BASE_DOWNLOAD_URL)
-                    .pathSegment(fileName)
-                    .build()
-                    .toUri();
-            try {
-                return downloadUri.toURL();
-            } catch (MalformedURLException e) {
-                throw new UncheckedIOException(e);
-            }
-        }
-    }
-
-    /**
-     * Downloads and unzips an IMDB data file and return the Path of the file if
-     * succesful.
-     */
-    public Path download(ImdbFile fileToDownload) {
-        Path tempFile = createTempFile(fileToDownload.getUnzippedFileName());
-
-        URL downloadURL = fileToDownload.getDownloadUrl();
-        try (InputStream unzippedStream = new GZIPInputStream(downloadURL.openStream()); // Unzip
-             ReadableByteChannel rbc = Channels.newChannel(unzippedStream);
-             FileOutputStream fos = new FileOutputStream(tempFile.toFile())
-        ) {
-            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-            logger.info("Downloaded file: {} to {}", downloadURL, tempFile);
-            return tempFile;
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
-
-    public Path createTempFile(String fileName) {
-        try {
-            return Files.createTempFile(fileName, ".tmp");
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+        public URI uri() {
+            return URI.create(DOWNLOAD_URL + "/" + name);
         }
     }
 }
