b9c03855d10f8a982e07ca5ccf49440588142d0c
Aria <amini5454@gmail.com>
2025-01-11 02:20:17 -0500

    Fix

--- infra/terraform/live/prod/main.tf
+++ infra/terraform/live/prod/main.tf
@@ -4,5 +4,6 @@ module "azure_vm" {
   location            = "eastus"
   name                = "imdbgraph-api"
   resource_group_name = "imdbgraph-rg"
+  # Public key (NOT SECRET)
   public_key          = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICESOYs4/kSLEyKzKPeKBou305mrqKSBZ5P3V4cuk47Z amini5454@gmail.com"
 }
