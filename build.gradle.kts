058d26826736ed128be6125639c6460c330342a4
Aria <amini5454@gmail.com>
2025-01-11 03:27:16 -0500

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,34 +100,6 @@ flyway {
     locations = arrayOf("classpath:db/migration")
 }
 
-task<Exec>("runAnsible") {
-    group = "infrastructure"
-
-    workingDir = File("infra/ansible")
-
-    environment("DATABASE_HOST", getEnv("DATABASE_HOST"))
-    environment("DATABASE_NAME", getEnv("DATABASE_NAME"))
-    environment("DATABASE_USER", getEnv("DATABASE_USER"))
-    environment("DATABASE_PASSWORD", getEnv("DATABASE_PASSWORD"))
-
-    environment("OMDB_KEY", getEnv("OMDB_KEY"))
-
-    commandLine("ansible", "-i", "staging", "main.yml")
-}
-
-task<Exec>("runTerraform") {
-    group = "infrastructure"
-
-    workingDir = File("infra/terraform/live/staging")
-
-    environment("ARM_CLIENT_ID", getEnv("ARM_CLIENT_ID"))
-    environment("ARM_CLIENT_SECRET", getEnv("ARM_CLIENT_SECRET"))
-    environment("ARM_SUBSCRIPTION_ID", getEnv("ARM_SUBSCRIPTION_ID"))
-    environment("ARM_TENANT_ID", getEnv("ARM_TENANT_ID"))
-
-    commandLine("terraform", "apply")
-}
-
 // ================================ HELPERS ====================================
 fun loadEnvFile(): Properties? {
     val properties = Properties()
