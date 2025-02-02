2e741e0769aa11c7aee700bd04cb15b2beb58296
Aria <amini5454@gmail.com>
2025-02-02 00:19:46 -0500

    Fix script

--- infra/terraform/bootstrap.sh
+++ infra/terraform/bootstrap.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-LOCATION=eastus
-
-# REPLACE WITH YOUR SUB ID!
-ARM_SUBSCRIPTION_ID="YOUR_ID"
+# REPLACE WITH YOUR SUB ID!!!!
+export ARM_SUBSCRIPTION_ID="11111111-1111-1111-1111-111111111111"
 
 # Set up credentials
 az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$ARM_SUBSCRIPTION_ID"
-# Copy the results returned from this command into the following env vars:
-# ARM_CLIENT_ID
-# ARM_CLIENT_SECRET
-# ARM_TENANT_ID
 
-# Set up storage account for storing terraform state files.
-RANDOM=$(openssl rand -hex 8)
-STORAGE_ACCOUNT_NAME="tfstate$RANDOM"
-RESOURCE_GROUP_NAME="tfstate"
+# Copy the results returned from this command into the following env vars:
+export ARM_CLIENT_ID=""
+export ARM_CLIENT_SECRET=""
+export ARM_TENANT_ID=""
 
 # Create resource group
-az group create --name "$RESOURCE_GROUP_NAME" --location "$LOCATION"
+az group create --name "$RESOURCE_GROUP_NAME" --location eastus
 
-# Create storage account
+# Create Storage Account.
+STORAGE_ACCOUNT_NAME="tfstate$(openssl rand -hex 8)"
+RESOURCE_GROUP_NAME="tfstate"
 az storage account create --resource-group "$RESOURCE_GROUP_NAME" --name "$STORAGE_ACCOUNT_NAME" --sku Standard_LRS --encryption-services blob
-
-# Create blob container
+# Create Blob Containers
 az storage container create --name "tfstate-staging" --account-name "$STORAGE_ACCOUNT_NAME"
 az storage container create --name "tfstate-prod" --account-name "$STORAGE_ACCOUNT_NAME"
