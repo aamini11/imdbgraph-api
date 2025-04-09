28aac01243939ae9ad9d435abbdd0051e7df79f1
Aria <amini5454@gmail.com>
2025-04-09 19:15:57 -0400

    delete outputs

--- infra/terraform/app/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.default.public_ip_address
-}
--- infra/terraform/env/staging/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip_address" {
-  value = module.app.public_ip_address
-}
