9f1bd9b4327857e9134b8f82ec5a4d7a176cc609
Aria <amini5454@gmail.com>
2025-04-11 14:19:35 -0400

    Cleanup terraform

--- infra/terraform/shared/main.tf
+++ infra/terraform/shared/main.tf
@@ -1,9 +1,11 @@
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_kubernetes_cluster" "default" {
+resource "azurerm_kubernetes_cluster" "this" {
   name                = "aks-imdbgraph"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
@@ -13,11 +15,41 @@ resource "azurerm_kubernetes_cluster" "default" {
     name       = "agentpool"
     node_count = 2
     vm_size    = "Standard_DS2_v2"
+
+    upgrade_settings {
+      drain_timeout_in_minutes      = 0
+      max_surge                     = "10%"
+      node_soak_duration_in_minutes = 0
+    }
   }
 
   identity {
     type = "SystemAssigned"
   }
+
+  oidc_issuer_enabled       = true
+  workload_identity_enabled = true
+}
+
+resource "azurerm_user_assigned_identity" "workload_id" {
+  name                = "workload-id"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_federated_identity_credential" "this" {
+  name                = azurerm_user_assigned_identity.workload_id.name
+  resource_group_name = azurerm_user_assigned_identity.workload_id.resource_group_name
+  parent_id           = azurerm_user_assigned_identity.workload_id.id
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = azurerm_kubernetes_cluster.this.oidc_issuer_url
+  subject             = "system:serviceaccount:imdbgraph:workload-identity-sa"
+}
+
+resource "azurerm_role_assignment" "key_vault_user" {
+  role_definition_name = "Key Vault Secrets User"
+  scope                = azurerm_key_vault.this.id
+  principal_id         = azurerm_user_assigned_identity.workload_id.principal_id
 }
 
 resource "azurerm_postgresql_flexible_server" "default" {
@@ -37,6 +69,36 @@ resource "azurerm_postgresql_flexible_server" "default" {
   # prevent the possibility of accidental data loss
   lifecycle {
     prevent_destroy = true
+  }
+}
+
+resource "azurerm_key_vault" "this" {
+  name                        = "kv-imdbgraph"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+
+  sku_name = "standard"
+  enabled_for_disk_encryption = true
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
   }
 }
 
